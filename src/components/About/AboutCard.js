6479781bd9b341619e9a8778ecac525401713a80
jaydeep bhattachary <jaydeep.bhattachary@rishabhsoft.com>
2024-10-10 14:53:53 +0530

    made portfolio web app

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,20 +8,19 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Jaydeep Bhattacharya </span>
+            from <span className="purple"> Kolkata, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a Associate Software Engineer at Rishabh Software Pvt Ltd.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed B.Tech in Computer Science & Engineering from UEM-Jaipur.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Chess
             </li>
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Jaydeep</footer>
         </blockquote>
       </Card.Body>
     </Card>
